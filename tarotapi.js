f13e5540b0456496b8494e0ddfcbcdf76962377a
nico0715 <147874351+nico0715@users.noreply.github.com>
2024-04-23 03:28:31 -0600

    end

---
+++ tarotapi.js
@@ -0,0 +1,18 @@
+
+fetch("https://quotes15.p.rapidapi.com/quotes/random/?language_code=en", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "quotes15.p.rapidapi.com",
+		"x-rapidapi-key": "a91e7d8405msh2f09846c9a2cec2p1f3937jsne998e0474d53"
+	}
+})
+.then(response => response.json())
+.then(response => {
+    console.log(response);
+    
+    document.getElementById('quote').innerHTML = response.content;
+    document.getElementById('author').innerHTML = '- ' + response.originator.name + ' -';
+})
+.catch(err => {
+	console.log(err);
+});
